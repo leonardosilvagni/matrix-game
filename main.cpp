f7b9b701a86506dec55a0621d8af32c62d7827e7
leonardosilvagni <leonardo.silvagni@gmail.com>
2018-07-11 14:48:15 +0200

    Some changes
    Made it playable with arrow on unix-like systems

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
-#include <iostream> 
+#include <iostream>
 #include <algorithm>//std::random_shuffle
 #include <ctime>	//std::time
 #include <cstdlib>  //std::rand, std::srand
 #include <termios.h>
 #include <cstdio>
-#define SPECIAL_KEY 0x1b
-#define A_KEY 97
-#define D_KEY 100 
-#define S_KEY 115
-#define W_KEY 119
-#define LEFT_ARROW_KEY 68 //0x25 ...0x28 
-#define UP_ARROW_KEY 65
-#define RIGHT_ARROW_KEY 67
-#define DOWN_ARROW_KEY 66
 using namespace std;
 static struct termios old, nw;
+
+const char SPECIAL_KEY 0x1b 	// <ESC> but in composed char like arrow the first part
+const char A_KEY 0x61
+const char D_KEY 0x64
+const char S_KEY 0x73
+const char W_KEY 0x77
+const char UP_ARROW_KEY 0x25		//65 ... 68
+const char DOWN_ARROW_KEY 0x26	//<ANY_ARROW> in Unix like are something like 0x1b
+const char RIGHT_ARROW_KEY 0x27
+const char LEFT_ARROW_KEY 0x28 //0x25 ...0x28
+
+
 
 /* Initialize new terminal i/o settings */
 void initTermios(int echo) {
@@ -31,7 +34,7 @@ void initTermios(int echo) {
 }
 
 /* Restore old terminal i/o settings */
-void resetTermios(void) 
+void resetTermios(void)
 {
   tcsetattr(0, TCSANOW, &old);
 }
@@ -76,7 +79,7 @@ void find_0(int matrix[4][4], int *x, int *y){
 	};
 }
 int aRandomValue(int i){return rand()%i;}
-int main(){	
+int main(){
 	int *mtx = new int[16]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 	int ctrl_matrix[4][4]={{mtx[0],mtx[1],mtx[2],mtx[3]},{mtx[4],mtx[5],mtx[6],mtx[7]}
 		,{mtx[8],mtx[9],mtx[10],mtx[11]},{mtx[12],mtx[13],mtx[14],mtx[15]},};
@@ -96,13 +99,48 @@ int main(){
 	print_2d_array(ctrl_matrix);
 	cout << "Now the playground have changed, you are the '0' and have to move \
 		to restore the playground to its initial arrangement\n";
-	print_2d_array(matrix);	
+	print_2d_array(matrix);
 	cout << "Press the arrow keys or 'WASD' to move and <ESC> to quit\n";
 	while(true){
 		char life = 0x00,chr_1 = 0x00, chr_2 = 0x00;
 		life = getch();
 		chr_1 = getch();
-		cher2 = getch();
+		chr_2 = getch();
+		if (life==SPECIAL_KEY&&chr_1==91){
+			if (chr_2==UP_ARROW_KEY){
+				if(x==0)break;
+				temp=matrix[x-1][y];
+				matrix[x-1][y]=0;
+				matrix[x][y]=temp;
+				x-=1;
+				moves++;
+
+			}if (chr_2==DOWN_ARROW_KEY){
+				if(x==0)break;
+				temp=matrix[x-1][y];
+				matrix[x-1][y]=0;
+				matrix[x][y]=temp;
+				x-=1;
+				moves++;
+
+			}if (chr_2==RIGHT_ARROW_KEY){
+				if(y==3)break;
+				temp=matrix[x][y+1];
+				matrix[x][y+1]=0;
+				matrix[x][y]=temp;
+				y+=1;
+				moves++;
+
+			}if (chr_2==LEFT_ARROW_KEY){
+				if(y==0)break;
+				temp=matrix[x][y-1];
+				matrix[x][y-1]=0;
+				matrix[x][y]=temp;
+				y-=1;
+				moves++;
+
+			}
+		}
 	/*	switch(life){
 			case DOWN_ARROW_KEY:
 			case S_KEY:
@@ -151,9 +189,9 @@ int main(){
 		}*/
 		system("clear");//Clear the screen
 		cout << "0 is in coordinates x: " << x+1 << " ; y: " << y+1 << endl;
-		cout << "Player moves: "<< moves << endl;	
+		cout << "Player moves: "<< moves << endl;
 		print_2d_array(matrix);
-			
+
 	}
 	return 0;
 }
